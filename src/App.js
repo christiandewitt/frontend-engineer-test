5b3285d4571a6fe6b535365b07006a0c9029056c
de Witt <christian.witt@wirecard.com>
2020-03-25 21:00:49 +0200

    added document title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './images/r7t-iso.svg';
 import './App.css';
 import { Provider } from 'react-redux';
+import Helmet from 'react-helmet';
 import store from './store'
 import Header from './components/Header/Header';
 import MapContainer from './components/Map/MapContainer';
@@ -10,6 +11,9 @@ function App() {
   return (
     <Provider store={store}>
       <div className="app">
+        <Helmet>
+          <title>Reconnect Test</title>
+        </Helmet>
         <Header title="Reconnect Test" logo={logo} />
         <MapContainer initialZoom={8} initialCenter={{lat: 35.091319, lng: -106.652455}}/>
       </div>
